f994286440995c9fc848fc8e4bda32bc49ef278a
Deeps <deepikkaa20@gmail.com>
2024-07-09 16:37:37 +0530

    done

---
+++ demo/src/main/java/com/wipro/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Vamsi Krishna. I am the spring boot Demo project");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
